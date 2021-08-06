364584fee7f655809bd175312a57bd9ba1ba9d05
Cas de Bruijn <cas.de.bruijn@student.ehb.be>
2021-08-06 02:52:49 +0200

    Aanzet POST request, sort functie en edit CSS

--- js/index.js
+++ js/index.js
@@ -1,12 +1,15 @@
 "use strict";
 console.log("test");
+let arrayKnoppenLikes = [];
+
 /*
 fetch('https://thecrew.cc/news/read.php')
     .then(response => response.json())
     .then(data => console.log("jen", data));
 */
 class Artikels {
-    constructor(artikelTitel, artikelFoto, artikelTekst, artikelLikes) {
+    constructor(artikelUUID, artikelTitel, artikelFoto, artikelTekst, artikelLikes) {
+        this.artikelUUID = artikelUUID;
         this.artikelTitel = artikelTitel;
         this.artikelFoto = artikelFoto;
         //this.artikelIntro = artikelIntro;
@@ -14,7 +17,7 @@ class Artikels {
         this.artikelLikes = artikelLikes;
     }
 }
-
+let arraySorted = [];
 let arrayArtikels = [];
 async function getData() {
     const response = await fetch('https://thecrew.cc/news/read.php');
@@ -27,12 +30,27 @@ async function getData() {
     //Vervolgens alle gegevens in een klasse plaatsen en deze klassen verzamelen in een array
     let arrayLengte = data.news.length;
     arrayArtikels = [];
+
     for (let i = 0; i < arrayLengte; i++) {
-        let artikel = new Artikels(data.news[i].title, data.news[i].imageURI, data.news[i].content, data.news[i].likes);
+        let artikel = new Artikels(data.news[i].UUID, data.news[i].title, data.news[i].imageURI, data.news[i].content, data.news[i].likes);
         arrayArtikels.push(artikel);
+        arraySorted.push(artikel);
     }
 
+    //Een extra array voor de sorted zaken
+    arraySorted.sort((a, b) => b.artikelLikes - a.artikelLikes);
     displayHTML(arrayArtikels);
+
+
+    /*Filter knoppen, aanpassen HTLM na klik op knop*/
+    document.getElementById('btnNoFilter').addEventListener('click', function () {
+        displayHTML(arrayArtikels);
+
+    });
+    document.getElementById('btnFilterLikes').addEventListener('click', function () {
+        displayHTML(arraySorted);
+
+    });
     return await data;
 }
 getData();
@@ -45,6 +63,10 @@ function displayHTML(dataArtikels) {
     dataArtikels.forEach((element) => {
         htmlInhoud += `
             <div class="flexboxItem">
+            <img src='./Icons/1200px-Heart_corazón.svg.png' class='iconHeart'>
+            <p class="likesTekst">${element.artikelLikes}</p>
+            <p class="likesTekst">${element.artikelUUID}</p>
+            <button class="likeKnop">Like</button>
             <h1 class="titel">${element.artikelTitel}</h1>
             <img src='${element.artikelFoto}' class="artikelFoto">
              <p class="artikelTekst">${element.artikelTekst}</p>
@@ -53,8 +75,31 @@ function displayHTML(dataArtikels) {
 
     });
 
+    containerElement.innerHTML = "";
     containerElement.innerHTML = htmlInhoud;
+    let likeKnoppen = document.getElementsByClassName('likeKnop');
+
+    for (let i = 0; i < likeKnoppen.length; i++) {
+        likeKnoppen[i].addEventListener('click', function () {
+            console.log("Hallo", dataArtikels[i].artikelUUID);
+            likeBlogPost(dataArtikels[i].artikelUUID);
+        });
+    }
+
+}
 
 
+function likeBlogPost(id) {
+    console.log(id);
+    fetch(`https://thecrew.cc/news/create.php`, {
+            method: 'POST',
+            body: JSON.stringify({
+                /*message: `{ "UUID": ${id}}`*/
+                id
+            })
+        })
 
+        .then(data => {
+            console.log("test", data);
+        });
 }
